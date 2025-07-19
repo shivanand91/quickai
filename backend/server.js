0b7e68055d4fae798bb88229f39de281dff61814
Shivanand Kumar <itzshivanand@gmail.com>
2025-07-19 22:18:11 +0530

    Backend project setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Server is Live!")
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    
+})
